82f0ebba7bafd41d247c39126cdf7059c72b0228
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-03-13 14:25:38 +0530

    Change in .json file and init folder

--- init/index.js
+++ init/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-Mongo_Url = "mongodb://127.0.0.1:27017/wanderlust"
+const Mongo_Url = "mongodb://127.0.0.1:27017/wanderlust"
 
 main()
 .then(()=>{
