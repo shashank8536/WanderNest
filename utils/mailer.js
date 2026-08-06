c087ac8e049fc7a8984f5c029abc2fe748556105
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-08-06 14:26:27 +0530

    Update transporter code

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // false for port 587
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
 });
 
 // 1. Send OTP for Signup Verification
