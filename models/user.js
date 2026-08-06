6c1b4ab8373a2f8484e75faeb402c31a0d2ffdd9
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-08-06 19:53:27 +0530

    email schema unique

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     username: {
         type: String,
