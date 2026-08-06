22499659261970eaa52246171eda83803b7c2f54
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-08-06 20:31:06 +0530

    Switch SMTP to port 587 with requireTLS to bypass Render outbound port 465 blocking

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,8 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     family: 4,
     auth: {
         user: process.env.EMAIL_USER,
